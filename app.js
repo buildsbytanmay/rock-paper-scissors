aea7916fef5fba966c9b55128576205a0e5560fe
buildsbytanmay <dushantb123@gmail.com>
2026-07-01 19:16:36 +0530

    show winner logic created

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ const drawGame = () => {
   msg.style.backgroundColor = "#081b31";
 };
 
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id");
