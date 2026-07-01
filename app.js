1696736e01b824c907e924eeec57385f8c915d1a
buildsbytanmay <dushantb123@gmail.com>
2026-07-01 18:52:46 +0530

    computer choice logic created

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
